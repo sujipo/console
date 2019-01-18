77ce166d3ae07932fac73f75f09b35c4f5b490e7
苏继坡 <sujp@sino-bridge.com>
2019-01-18 13:56:18 +0800

    修改main.go，默认okd改为ocp

--- cmd/bridge/main.go
+++ cmd/bridge/main.go
@@ -82,7 +82,7 @@ func main() {
 	fDeveloperConsoleURL := fs.String("developer-console-url", "", "URL for the developer console used in masthead context switcher. This option will be removed in a future release.")
 
 	fDexAPIHost := fs.String("dex-api-host", "", "Target host and port of the Dex API service.")
-	fBranding := fs.String("branding", "okd", "Console branding for the masthead logo and title. One of okd, ocp, online, or dedicated. Defaults to okd.")
+	fBranding := fs.String("branding", "ocp", "Console branding for the masthead logo and title. One of okd, ocp, online, or dedicated. Defaults to okd.")
 	fDocumentationBaseURL := fs.String("documentation-base-url", "", "The base URL for documentation links.")
 	fGoogleTagManagerID := fs.String("google-tag-manager-id", "", "Google Tag Manager ID. External analytics are disabled if this is not set.")
 
